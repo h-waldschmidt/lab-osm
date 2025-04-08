6d79a374106a79c00e75383db22fe61723b9e54d
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2025-04-08 16:57:20 +0200

    merge ways

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,25 @@
-#include <osmium/io/any_input.hpp>
-#include <osmium/handler.hpp>
-#include <osmium/visitor.hpp>
-#include <osmium/index/map/sparse_mem_array.hpp>
-#include <osmium/handler/node_locations_for_ways.hpp>
-#include <osmium/osm/location.hpp>
-
-#include <unordered_map>
-#include <vector>
-#include <iostream>
+#include <cstdint>
 #include <fstream>
-#include <utility>
+#include <iostream>
+#include <osmium/handler.hpp>
+#include <osmium/handler/node_locations_for_ways.hpp>
+#include <osmium/index/map/sparse_mem_array.hpp>
+#include <osmium/io/any_input.hpp>
+#include <osmium/osm/location.hpp>
+#include <osmium/visitor.hpp>
 #include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 using NodeMap = std::unordered_map<uint64_t, std::pair<double, double>>;
-using WayList = std::vector<std::vector<uint64_t>>;
+using WayList = std::unordered_map<uint64_t, std::vector<uint64_t>>;
 
 NodeMap coastline_nodes;
 WayList coastline_ways;
 
 class CoastlineHandler : public osmium::handler::Handler {
-public:
+   public:
     void way(const osmium::Way& way) {
         if (way.tags().has_tag("natural", "coastline")) {
             std::vector<uint64_t> way_nodes;
@@ -32,7 +32,7 @@ public:
                 }
             }
             if (!way_nodes.empty()) {
-                coastline_ways.push_back(std::move(way_nodes));
+                coastline_ways[way_nodes.front()] = way_nodes;
             }
         }
     }
@@ -48,8 +48,9 @@ void write_geojson(const std::string& output_file, const NodeMap& nodes, const W
         first = false;
 
         out << R"({"type": "Feature","geometry":{"type": "LineString","coordinates":[)";
-        for (size_t i = 0; i < way.size(); ++i) {
-            uint64_t id = way[i];
+        const auto& way_nodes = way.second;
+        for (size_t i = 0; i < way_nodes.size(); ++i) {
+            uint64_t id = way_nodes[i];
             auto it = nodes.find(id);
             if (it != nodes.end()) {
                 const auto& [lon, lat] = it->second;
@@ -60,7 +61,29 @@ void write_geojson(const std::string& output_file, const NodeMap& nodes, const W
         out << R"(]},"properties":{}})";
     }
 
-    out << "]}" << std::endl;
+    out << "]}" << '\n';
+}
+
+void merge_ways() {
+    size_t old_size = coastline_ways.size();
+    while (true) {
+        for (auto it1 = coastline_ways.begin(); it1 != coastline_ways.end(); ++it1) {
+            uint64_t last_id = it1->second.back();
+            if (last_id == it1->first) continue;
+
+            auto it2 = coastline_ways.find(last_id);
+            if (it2 != coastline_ways.end()) {
+                it1->second.insert(it1->second.end(), it2->second.begin(), it2->second.end());
+                coastline_ways.erase(it2);
+            }
+        }
+
+        if (coastline_ways.size() == old_size || coastline_ways.size() == 1) {
+            break;
+        }
+
+        old_size = coastline_ways.size();
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -71,6 +94,8 @@ int main(int argc, char* argv[]) {
 
     const char* input_filename = argv[1];
     const char* output_filename = argv[2];
+
+    auto start_reading = std::chrono::high_resolution_clock::now();
 
     osmium::io::Reader reader{input_filename, osmium::osm_entity_bits::node | osmium::osm_entity_bits::way};
 
@@ -83,6 +108,30 @@ int main(int argc, char* argv[]) {
 
     osmium::apply(reader, location_handler, handler);
     reader.close();
+    auto end_reading = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Reading time: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end_reading - start_reading).count() << " ms\n";
+
+    auto start_merging = std::chrono::high_resolution_clock::now();
+    merge_ways();
+    auto end_merging = std::chrono::high_resolution_clock::now();
+    std::cout << "Merging time: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end_merging - start_merging).count() << " ms\n";
+
+    /*
+    // find the longest array
+    size_t max_size = 0;
+    std::vector<uint64_t> longest_way;
+    for (const auto& way : coastline_ways) {
+        if (way.second.size() > max_size) {
+            max_size = way.second.size();
+            longest_way = way.second;
+        }
+    }
+    coastline_ways.clear();
+    coastline_ways[longest_way.front()] = longest_way;
+    */
 
     write_geojson(output_filename, coastline_nodes, coastline_ways);
 
